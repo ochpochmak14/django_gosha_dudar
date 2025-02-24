c68006021b0ade9c669859ac2515a78e12f6c78b
Emin <makhmudovemin441@gmail.com>
2025-02-24 19:58:36 +0500

    dict argument in render func

--- website/main/views.py
+++ website/main/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    dct = {
+        'obj': {
+        'Car': 'BMW',
+        'Owner': 'Emin',
+        'MarketCap': '3.000.000$',
+        'title': 'Главная страница!!'
+        }
+    }
+    return render(request, 'main/index.html', dct)
 
 def about(request):
     return render(request, 'main/about.html')
